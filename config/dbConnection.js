c8c9e949cc8f6b385ae8855f791c4b1332f592af
sourav@own <sourav.ion@gmail.com>
2022-08-19 01:26:49 +0530

    modified db config

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,15 +1,22 @@
-const {mongooes,  ServerApiVersion} = require("mongoose")
+const mongooes = require("mongoose")
 require('dotenv').config({path: '.env'});
 const url = process.env.Url;
 function databseConnection (){
 
-    mongooes.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }).then(()=>{
+    mongooes.connect(url).then(()=>{
     console.log('DataBase Connected successfully')
     }).catch((error)=>{
     console.log(error);
 })
 }
-
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://koniat:<password>@koniatdb.ynfjwur.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 module.exports = {
     databseConnection
 }
