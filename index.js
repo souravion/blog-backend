4977eadba03d816b93a55ffd32972c94e8161eef
sourav@own <sourav.ion@gmail.com>
2022-08-20 12:46:12 +0530

    add configuration

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use('/admin',adminRouter)
 
 app.use(notFoundHandler);
 app.use(defaultErrorHandler);
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8080,()=>{
     console.log(`Server listening on ${process.env.PORT}`)
