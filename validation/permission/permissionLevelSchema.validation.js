57aef5c7f9e8bb5cb4b06c2d4c5c3b476f889b94
sourav@own <sourav.ion@gmail.com>
2022-10-08 14:07:35 +0530

    validation add

--- validation/permission/permissionLevelSchema.validation.js
+++ validation/permission/permissionLevelSchema.validation.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 exports.AddPermissionLevel = Joi.object({
     name:Joi.string().required(),
-    parentId:Joi.number().required(),
+    parentId:Joi.required(),
     options:Joi.object().required()
 })
