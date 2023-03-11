23241f3979978248f1dd14eaeb826b5e38e2b5b2
sourav@ion <sourav.ion@gmail.com>
2023-03-11 15:40:35 +0530

    remove console loge

--- services/admin/category.service.js
+++ services/admin/category.service.js
@@ -92,8 +92,6 @@ exports.FindCategory = async(req,res)=>{
 
 
 exports.ChangeStatus = async(id,req,res)=>{
-    console.log(id)
-    console.log(req)
     try{
         const ChangedStatus = await Category.findByIdAndUpdate(id,req);
         return ChangedStatus
